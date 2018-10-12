a04683642d9917489a01e91a56c5c9ddb5de0c71
dradkowski <cyberdogpolska@gmail.com>
2018-10-12 10:56:31 +0200

    drugi scanf

--- main.c
+++ main.c
@@ -3,11 +3,19 @@
 int main() {
     char znak;
 
-    printf("podaj znak ");
+    int wartosc_calkowita;
+    double wartosc_wymierna;
 
+    printf("podaj znak ");
     scanf("%c", &znak);
 
-    putchar(znak);
+    printf("podaj liczbe calkowita ");
+    scanf(" %i", &wartosc_calkowita);
+
+    printf("podaj liczbe zmiennoprzecinkow a");
+    scanf(" %lf", &wartosc_wymierna);
+
+    printf("znak to %c, liczba calkowita to %i, liczba zmiannoprzecinkowa to %lf", znak, wartosc_calkowita, wartosc_wymierna);
 
     return 0;
 }
